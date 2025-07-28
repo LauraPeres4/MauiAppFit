a7e1f80ab7cc824f69e8ebaa7969f9b9029cf51f
231402 <231402@CPD03-M19>
2025-07-28 15:54:17 -0300

    monark rei

--- MauiAppFit/App.xaml.cs
+++ MauiAppFit/App.xaml.cs
@@ -1,15 +1,30 @@
-﻿namespace MauiAppFit
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+﻿namespace MauiAppFit.Helpers;
 
-        protected override Window CreateWindow(IActivationState? activationState)
+public partial class App : Application
+{
+    static SQLiteDataBaseHelper database;
+
+    public static SQLiteDataBaseHelper Database
+    {
+        get
         {
-            return new Window(new AppShell());
-        }
+            if (database == null)
+            {
+                database = new SQLiteDataBaseHelper(
+                    Path.Combine(Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData),
+                        "XamAppFit.db3"));
+            }
+            return database;
+        } // FECHA DATABASE...
+    }
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
     }
 }
